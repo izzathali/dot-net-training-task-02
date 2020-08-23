67198d51f82bd6a908feb875a91ef75f90b9abce
Izzath Ali <izzath.info@gmail.com>
2020-08-24 00:51:10 +0530

    RentCalculation Updated

--- ABC_Drive/ABC_Drive/Driver/frmDriver.Designer.cs
+++ ABC_Drive/ABC_Drive/Driver/frmDriver.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbLicence = new System.Windows.Forms.ComboBox();
             this.txtDriverCost = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
@@ -42,6 +44,8 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.cmbLicence);
             this.panel1.Controls.Add(this.txtDriverCost);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.label2);
@@ -51,28 +55,48 @@
             this.panel1.Location = new System.Drawing.Point(2, -3);
             this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(644, 136);
+            this.panel1.Size = new System.Drawing.Size(826, 136);
             this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(333, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Licence Type:";
+            // 
+            // cmbLicence
+            // 
+            this.cmbLicence.FormattingEnabled = true;
+            this.cmbLicence.Items.AddRange(new object[] {
+            "Light Vehicles",
+            "Heavy Duty Vehicles"});
+            this.cmbLicence.Location = new System.Drawing.Point(337, 64);
+            this.cmbLicence.Name = "cmbLicence";
+            this.cmbLicence.Size = new System.Drawing.Size(188, 28);
+            this.cmbLicence.TabIndex = 2;
             // 
             // txtDriverCost
             // 
-            this.txtDriverCost.Location = new System.Drawing.Point(371, 66);
+            this.txtDriverCost.Location = new System.Drawing.Point(569, 66);
             this.txtDriverCost.Name = "txtDriverCost";
             this.txtDriverCost.Size = new System.Drawing.Size(219, 26);
-            this.txtDriverCost.TabIndex = 5;
+            this.txtDriverCost.TabIndex = 3;
             // 
             // panel2
             // 
-            this.panel2.Location = new System.Drawing.Point(1, 127);
+            this.panel2.Location = new System.Drawing.Point(1, 133);
             this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(643, 66);
+            this.panel2.Size = new System.Drawing.Size(821, 60);
             this.panel2.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(366, 37);
+            this.label2.Location = new System.Drawing.Point(564, 37);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(151, 20);
             this.label2.TabIndex = 4;
@@ -83,7 +107,7 @@
             this.txtDriverName.Location = new System.Drawing.Point(69, 66);
             this.txtDriverName.Name = "txtDriverName";
             this.txtDriverName.Size = new System.Drawing.Size(219, 26);
-            this.txtDriverName.TabIndex = 3;
+            this.txtDriverName.TabIndex = 1;
             // 
             // label1
             // 
@@ -96,20 +120,20 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(499, 145);
+            this.btnSave.Location = new System.Drawing.Point(698, 145);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(93, 29);
-            this.btnSave.TabIndex = 1;
+            this.btnSave.TabIndex = 4;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(397, 145);
+            this.btnCancel.Location = new System.Drawing.Point(596, 145);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(93, 29);
-            this.btnCancel.TabIndex = 2;
+            this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
@@ -118,7 +142,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 186);
+            this.ClientSize = new System.Drawing.Size(826, 186);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.panel1);
@@ -129,6 +153,7 @@
             this.Name = "frmDriver";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add Driver";
+            this.Load += new System.EventHandler(this.frmDriver_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -145,5 +170,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbLicence;
     }
 }
---
+++ ABC_Drive/ABC_Drive/Migrations/202008231243290_UpdateLicence.cs
@@ -0,0 +1,18 @@
+﻿namespace ABC_Drive.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateLicence : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Drivers", "LicenceType", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Drivers", "LicenceType", c => c.Int(nullable: false));
+        }
+    }
+}
